cf2688eb76d9cd0ca18457ce658ab98c73e52081
Jared Wahlstrand <jwahlstrand@gmail.com>
2022-12-29 08:18:13 -0500

    add second script for code generation and a README for that

---
+++ gen/gen_rest.jl
@@ -0,0 +1,9 @@
+using GI, EzXML
+GI.prepend_search_path("/usr/lib64/girepository-1.0")
+include("gen_pango.jl")
+include("gen_pangocairo.jl")
+include("gen_graphene.jl")
+include("gen_gsk.jl")
+include("gen_gdk4.jl")
+include("gen_gtk4.jl")
+#include("gen_adwaita.jl")
