f609c58a3631609fddc51f75f61ea02a228d5e70
Jared Wahlstrand <jwahlstrand@gmail.com>
2023-11-05 20:27:21 -0500

    try to diagnose dialog crash

--- test/gui/dialogs.jl
+++ test/gui/dialogs.jl
@@ -1,23 +1,54 @@
 using Test, Gtk4
 
-@testset "dialogs" begin
+@testset "ask dialog" begin
 
 main_window = GtkWindow("Dialog example")
 
 r = ask_dialog("May I ask you a question?",main_window; timeout = 0.25)
 @test r == false
 
+sleep(1.0)
+
+destroy(main_window)
+
+end
+
+@testset "info dialogs" begin
+
+main_window = GtkWindow("Dialog example")
+
 info_dialog("Here's some information", main_window; timeout = 0.25)
+sleep(1.0)
 warn_dialog("Here's some alarming information", main_window; timeout = 0.25)
+sleep(1.0)
 error_dialog("Here's an error", main_window; timeout = 0.25)
+sleep(1.0)
+GC.gc()
+sleep(1.0)
+destroy(main_window)
+
+end
+
+@testset "file dialogs" begin
+
+main_window = GtkWindow("Dialog example")
 
 open_dialog("Pick a file to open", main_window; timeout = 0.25)
+sleep(1.0)
 open_dialog("Pick a file to open", main_window, ["*.png"]; timeout = 0.25)
+sleep(1.0)
+
 save_dialog("Pick a filename", main_window; timeout = 0.25)
+
+sleep(1.0)
 
 color_dialog("What is your favorite color?", main_window; timeout = 0.25)
 
+sleep(1.0)
+
 input_dialog("What is the meaning of life, the universe, and everything?", "42", (("Cancel", 0), ("Accept", 1)), main_window; timeout = 0.25)
+
+sleep(1.0)
 
 GC.gc() # ensure GtkDialog is really gone before main_window
 
