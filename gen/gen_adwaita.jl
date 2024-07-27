e258ffaa5a493781798940c7289d8ade03028b67
Jared Wahlstrand <jwahlstrand@gmail.com>
2024-07-27 14:12:21 -0400

    simplify generation script for Adwaita

--- gen/gen_adwaita.jl
+++ gen/gen_adwaita.jl
@@ -1,22 +1,9 @@
 using GI
 
-toplevel, exprs, exports = GI.output_exprs()
-
 ns = GINamespace(:Adw,"1")
 path="../Adwaita/src/gen"
 
-## constants, enums, and flags, put in a "Constants" submodule
-
-const_mod = Expr(:block)
-
-const_exports = Expr(:export)
-
-GI.all_const_exprs!(const_mod, const_exports, ns)
-
-push!(exprs, const_mod)
-
-## export constants, enums, and flags code
-GI.write_to_file(path,"adw_consts",toplevel)
+GI.export_consts!(ns, path, "adw"; export_constants = false)
 
 ## structs
 
@@ -42,30 +29,8 @@ push!(exprs,exports)
 
 GI.write_to_file(path,"adw_structs",toplevel)
 
-## struct methods
-
-toplevel, exprs, exports = GI.output_exprs()
-
-skiplist=[]
-
-GI.all_struct_methods!(exprs,ns,print_detailed=true,skiplist=skiplist,struct_skiplist=struct_skiplist)
-
-## object methods
-
 skiplist=[:get_expression,:set_expression,:add_breakpoint,:get_current_breakpoint]
 
-# skips are to avoid method name collisions
-GI.all_object_methods!(exprs,ns;skiplist=skiplist,object_skiplist=obj_skiplist, exclude_deprecated=false)
+GI.export_methods!(ns, path, "adw"; struct_skiplist = struct_skiplist, object_method_skiplist = skiplist, object_skiplist = obj_skiplist, exclude_deprecated=false)
 
-# skips are to avoid method name collisions
-GI.all_interface_methods!(exprs,ns)
-
-GI.write_to_file(path,"adw_methods",toplevel)
-
-## functions
-
-toplevel, exprs, exports = GI.output_exprs()
-
-GI.all_functions!(exprs,ns;skiplist=[:breakpoint_condition_parse])
-
-GI.write_to_file(path,"adw_functions",toplevel)
+GI.export_functions!(ns, path, "adw"; skiplist=[:breakpoint_condition_parse])
