54fe62c5110e840870995a866ebb177099f7d337
Neven Sajko <s@purelymail.com>
2025-09-08 05:21:07 +0200

    prevent adding methods to the functions `>` and `>=`
    As documented, the intended way to implement `>` is to add a method to
    `<`. Similarly with `>=`. A package should never add a method to either
    `>` or `>=`.

--- src/text.jl
+++ src/text.jl
@@ -152,8 +152,6 @@ end
 Base.:(==)(lhs::TI, rhs::TI) = G_.equal(lhs, rhs)
 Base.:(<)(lhs::TI, rhs::TI) = G_.compare(lhs, rhs) < 0
 Base.:(<=)(lhs::TI, rhs::TI) = G_.compare(lhs, rhs) <= 0
-Base.:(>)(lhs::TI, rhs::TI) = G_.compare(lhs, rhs) > 0
-Base.:(>=)(lhs::TI, rhs::TI) = G_.compare(lhs, rhs) >= 0
 
 start_(iter::TI) = Ref(iter)
 iterate(::TI, iter=start_(iter)) = iter.is_end ? nothing : (iter.char, iter + 1)
